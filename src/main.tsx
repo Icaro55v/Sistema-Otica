3e4daa81ec0fd424ffd7cc016f9d1e0f92d81a47
Icaro55v <Icarohenrrique99@gmail.com>
2026-09-05 01:32:07 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-// IMPORTANTE: Ajuste o caminho do import do App se o nome do arquivo for diferente no seu projeto local.
-// No nosso ambiente ele se chama tica_vis_o_erp_completo_e_modular (2).tsx, mas localmente você deve renomear para App.tsx
-import App from './App.tsx' 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
