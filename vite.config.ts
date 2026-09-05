ae68a4f10b9b82dc3711068ad2cf4ba107282265
Icaro55v <Icarohenrrique99@gmail.com>
2026-09-05 01:31:10 -0300

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ISSO AQUI É O QUE RESOLVE O ERRO 404
-  // O nome aqui tem que ser EXATAMENTE igual ao nome do seu repositório no GitHub (com maiúsculas e minúsculas)
   base: '/Sistema-Otica/',
 });
