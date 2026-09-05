464d6627a9967925824d62bf26df4a56da50f878
Icaro55v <Icarohenrrique99@gmail.com>
2026-09-05 01:15:14 -0300

    Fix base path in Vite config for GitHub deployment
    Updated base path to match GitHub repository name.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ATUALIZADO: Usando exatamente as letras maiúsculas do seu link
+  // ISSO AQUI É O QUE RESOLVE O ERRO 404
+  // O nome aqui tem que ser EXATAMENTE igual ao nome do seu repositório no GitHub (com maiúsculas e minúsculas)
   base: '/Sistema-Otica/',
 });
